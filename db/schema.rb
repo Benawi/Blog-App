2d08781a7eebd4538003080d77caa28dbf40d48a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-29 15:31:57 +0300

    Migrate create comments active record

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_122330) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_122901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "post_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_122330) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
