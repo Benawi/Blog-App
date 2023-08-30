39e03623717dcb88367fce05799021a63abff818
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-30 15:35:26 +0300

    Add most recent posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+  def most_recent_posts
+    posts = Post.all
+    posts.order(created_at: :desc).limit(3)
+  end
 end
