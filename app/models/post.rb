7584e1db974f92f59d5bd008334847d2857af9c7
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-30 15:41:37 +0300

    Add most recent comments  method

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,7 @@ class Post < ApplicationRecord
         likes_counter = Like.where(post_id: id).count
         update(likes_counter: likes_counter)
     end
+    def most_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
 end
