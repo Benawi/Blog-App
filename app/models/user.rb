088b594dbcafe7412ddfd98f1a9d13870e4570d8
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-30 15:34:39 +0300

    Add attributies of user class and create association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
+  has_many :likes, foreign_key: 'author_id', class_name: 'Like'
 end
