563ca3060cf992bcabbf13f138866deef2f671aa
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-30 16:38:42 +0300

    Update properties name

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :editor, class_name: 'User'
+  belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'post_id', class_name: 'Like'
 
