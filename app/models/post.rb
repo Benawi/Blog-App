0281994dab11fb007c871d94ff03e802cbdf1ef0
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-30 15:40:10 +0300

    Add updte comment counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
     belongs_to :editor, class_name: 'User'
     has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
     has_many :likes, foreign_key: 'post_id', class_name: 'Like'
+
+    def update_comments_counter
+        comments_counter = Comment.where(post_id: id).count
+        update(comments_counter: comments_counter)
+    end
 end
