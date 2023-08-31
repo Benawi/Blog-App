b20b8821c7d158a0a7cc4afac78eaf5e7d6f9b00
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-31 20:44:01 +0300

    Add test case for is valid with valid attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   subject do
+    User.new(name: 'Anything', bio: 'Lorem ipsum', photo: 'https://ui-avatars.com/api/?name=sara&background=random',
+             posts_counter: 1)
+  end
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
 end
