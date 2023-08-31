747041461987c51c64ae89fef31709fd9373bb3b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-31 21:03:11 +0300

    fix linters bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_save :update_comments_count
+
+  validates :text, presence: true, length: { minimum: 1 }
+
+  def update_comments_count
+    post.increment!(:comments_counter)
+  end
 end
