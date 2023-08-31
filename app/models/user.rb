2f01072bd63f8aa9efd09652acf29920831be497
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-31 15:58:58 +0300

    Add validation PostsCounter must be an integer greater than or equal to zero.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
 
   validates :name, presence: true
-  git
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # Callback
   after_save :most_recent_posts
   after_save :update_posts_counter
   def most_recent_posts
