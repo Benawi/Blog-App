6101722df609f921611efa574c78ac3a416069b7
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-31 20:44:25 +0300

    Add test case for is not valid without a name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+ 
 end
