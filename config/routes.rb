f561a10b59fab673c9b2d486d97b27dc985a5c3e
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-04 18:31:44 +0300

    add routes for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:index, :show] do
+    resources :posts ,only:[:index,:show], controller: 'user_posts'
+  end
+  root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
