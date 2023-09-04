0bd1a7d0a8eb8ca54fa343955020616efe9a0722
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-04 18:52:42 +0300

    Fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :users, only: [:index, :show] do
-    resources :posts ,only:[:index,:show], controller: 'user_posts'
-  end
   root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :create]
+  end
+
 end
