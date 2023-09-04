4fdd9f433db3c0d67929a1fd264019bc5bd05239
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-04 20:47:26 +0300

    Update the route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create]
+    resources :posts, only: [:index, :show]
   end
 
 end
