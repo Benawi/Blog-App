23f76a87640ab1b5dd9044f71b241132fffd0e4e
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-05 17:18:13 +0300

    Add test cases for posts per users

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -22,5 +22,25 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
-  
+  describe 'GET /users/:user_id/posts/:id' do
+    before :each do
+      get '/users/2/posts/2'
+    end
+
+    it 'should work correctly' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders show template correctly' do
+      expect(response).to render_template(:show)
+    end
+
+    it "doesn't renders template other than /users/:user_id/posts/:id" do
+      expect(response).to_not render_template(:index)
+    end
+
+    it 'shows the correct placeholder text' do
+      expect(response.body).to include('Here is the detail of individual comments')
+    end
+  end
 end
