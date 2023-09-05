de64dab2abfc0965df58bb086d7a7baf5a611df8
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-05 16:56:49 +0300

    Add test case for shows the correct info with id

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -21,5 +21,27 @@ RSpec.describe 'Users', type: :request do
     it 'shows the correct placeholder text' do
       expect(response.body).to include('User was successfully created.')
     end
-  
+  end
+
+  describe 'GET /users/:id' do
+    before :each do
+      get user_path(2)
+    end
+
+    it 'should work correctly' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders show template correctly' do
+      expect(response).to render_template(:show)
+    end
+
+    it "doesn't renders template other than /users/:id" do
+      expect(response).to_not render_template(:index)
+    end
+
+    it 'shows the correct placeholder text' do
+      expect(response.body).to include('On Construction')
+    end
+  end
 end
