f4bd20ba260db9c25dac77dbe1543acdc130cc12
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-07 23:04:50 +0300

    Add comment adder

--- app/controllers/comment_controller.rb
+++
@@ -1,6 +0,0 @@
-class CommentsController < ApplicationController
-  def create
-    comment = post.comments.create(comment_params.merge(user_id: current_user.id))
-    respond_with post, comment
-  end
-end
