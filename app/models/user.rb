2a484eab72bd28c2b04d150d6cc4c28ee882b5b9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-08 12:22:33 +0300

    refactor the models code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,25 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id, class_name: 'Post'
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :comments, foreign_key: 'author_id'
+  # counter_cache makes sure Rails automatically update the PostCounter
+  # attribute in the User model when posts are created or destroyed.
+  has_many :posts, foreign_key: 'author_id', counter_cache: true
+  has_many :likes, foreign_key: 'author_id'
 
+  # The code `validates :Name, presence: true` is validating that the `Name`
+  # attribute of the `User` model is present, meaning it cannot be blank or nil.
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # Callback
-  after_save :most_recent_posts
-  def most_recent_posts
-    posts = Post.all
-    posts.order(created_at: :desc).limit(3)
+  # is adding a validation to the `User` model. It ensures that the `PostCounter` attribute is a
+  # numerical value and that it is greater than or equal to 0. This means that the `PostCounter`
+  # attribute cannot be a non-integer value or a negative number.
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # The function retrieves the three most recent posts written
+  # by the author.
+  def three_most_recent_posts
+    Post.where(author_id: id).order(created_at: :desc).limit(3)
   end
 end
+
+# Code to verify if the three_most_recent_posts method is working
+# user = User.find_by(Name: "Kwame")
+# user.three_most_recent_posts
