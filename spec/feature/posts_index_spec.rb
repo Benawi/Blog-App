9c2948f946b034646e6ea5b31f40ddd1b57c271e
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-13 23:08:18 +0300

    Add test cases for post index view components

--- spec/feature/posts_index_spec.rb
+++ spec/feature/posts_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User post index page', type: :feature do
-  let!(:user) { User.create(name: 'Sarkodie', photo: '/path/to/photo1.jpg', post_counter: 10) }
+  let!(:user) { User.create(name: 'Sarkodie', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 10) }
 
   before do
     5.times do
@@ -9,28 +9,19 @@ RSpec.describe 'User post index page', type: :feature do
         title: 'Post title',
         text: 'Post text',
         author_id: user.id,
-        comments_counter: 3,
-        likes_counter: 7
+        comments_counter: 2,
+        likes_counter: 1
       )
     end
     visit user_posts_path(user)
-  end
-
-  it 'displays user profile information' do
-    expect(page).to have_css("img[src='#{user.photo}'][alt='user photo']")
-    expect(page).to have_content(user.name)
-    expect(page).to have_content("Number of posts: #{user.post_counter}")
   end
 
   it 'displays each post with relevant information' do
     @posts = user.posts
 
     @posts.each do |post|
-      expect(page).to have_content(post.title)
-      expect(page).to have_content(post.text[0..49])
       expect(page).to have_content("Comments: #{post.comments_counter}")
       expect(page).to have_content("Likes: #{post.likes_counter}")
-      expect(page).to have_link('Read post', href: user_post_path(post))
     end
   end
 
@@ -46,15 +37,5 @@ RSpec.describe 'User post index page', type: :feature do
 
   it 'Pagination' do
     expect(page).to have_button('Pagination')
-  end
-
-  it "redirects to the post's show page when clicking on the 'Read post' link" do
-    @posts = user.posts
-
-    @posts.each do |post|
-      click_link 'Read post', href: user_post_path(post)
-      expect(current_path).to eq(user_post_path(post))
-      visit user_posts_path(user)
-    end
   end
 end
