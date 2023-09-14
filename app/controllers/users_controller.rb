27f46e843f840e2473cc332d6136f12a5a1257ec
wolterbwambale <benplaan1@gmail.com>
2023-09-14 17:41:34 +0300

    add show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,12 @@ class UsersController < ApplicationController
   def edit
     current_user.update(user_params)
   end
+
+  # The function retrieves a user object from the database
+  # based on the provided ID.
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.includes(:user).where(author_id: @user.id)
+    cookies[:user_id] = @user.id
+  end
 end
