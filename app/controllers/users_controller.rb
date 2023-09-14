8a9c05df051896e2c011e966d112c7757a739be3
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-14 17:50:16 +0300

    Refactor the controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def edit
+    current_user.update(user_params)
+  end
+
   # The function retrieves a user object from the database
   # based on the provided ID.
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = Post.includes(:user).where(author_id: @user.id)
+    cookies[:user_id] = @user.id
   end
 end
