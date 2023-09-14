526950290a362322c69068564f475ef864720e40
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-14 18:26:10 +0300

    user confirmable added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:confirmable
   # Include default devise modules. Others available are
   has_many :comments, foreign_key: 'author_id'
   # counter_cache makes sure Rails automatically update the PostCounter
