e5e699a016a86716cc61ff7734d0c252a3d88bf0
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-15 10:07:22 +0300

    Refactor comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,10 @@ class Comment < ApplicationRecord
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
+  
+  def decrement_comment_count
+    post.decrement!(:comments_counter)
+  end
 end
 
 # Code to verify if the update_post_comments_counter method is working
